d423a353ed400c5c370f3e276dec2a3e475156c5
Rendy Eka <linahonduras@protonmail.com>
2022-05-19 10:48:43 +0700

    Menambahkan aplikasi java swing

---
+++ src/main/java/com/mycompany/bakso_gaul/harga.java
@@ -0,0 +1,45 @@
+package com.mycompany.bakso_gaul;
+
+public class harga {
+
+    private Integer rudal = 25000;
+    private Integer beranak = 22000;
+    private Integer tumpeng = 32000;
+    private Integer ayam_hot = 18000;
+    private Integer ayam_bakso = 28000;
+    private Integer teh = 5000;
+    private Integer kelapa = 12000;
+    private Integer jeruk = 5000;
+  
+    public Integer getRudal() {
+        return rudal;
+    }
+
+    public Integer getBeranak() {
+        return beranak;
+    }
+
+    public Integer getTumpeng() {
+        return tumpeng;
+    }
+
+    public Integer getAyam_hot() {
+        return ayam_hot;
+    }
+
+    public Integer getAyam_bakso() {
+        return ayam_bakso;
+    }
+
+    public Integer getTeh() {
+        return teh;
+    }
+
+    public Integer getKelapa() {
+        return kelapa;
+    }
+
+    public Integer getJeruk() {
+        return jeruk;
+    }
+}
